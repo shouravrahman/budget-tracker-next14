8371159f4c441046f648d6b346e7a6fd72e140e5
shouravrahman <shouravrahman006@gmail.com>
2024-05-10 02:25:59 +0600

    register backend

--- actions/register.ts
+++ actions/register.ts
@@ -1,17 +1,36 @@
 "use server"
-
+import bcrypt from 'bcrypt'
 import { registerSchema } from "@/schemas"
 import { z } from "zod"
+import db from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 export const register = async (data: z.infer<typeof registerSchema>) => {
 
-// server side validation
+   // server side validation
    const validatedData = registerSchema.safeParse(data)
 
-   if(!validatedData.success){
-   //  return validatedData.error
-    return {error: "Invalid fields"}
-}
-    return {success: "Email sent"}
- 
+   if (!validatedData.success) {
+      //  return validatedData.error
+      return { error: "Invalid fields" }
+   }
+
+   const { email, password, name } = validatedData.data;
+   const hashedPassword = await bcrypt.hash(password, 10)
+
+   const existingUser = await getUserByEmail(email)
+
+   if (existingUser) {
+      return { error: "Email already in use" }
+   }
+
+   await db.user.create({
+      data: {
+         name, email, password: hashedPassword
+      }
+   })
+   // todo send verification
+
+   return { success: "User created" }
+
 }
